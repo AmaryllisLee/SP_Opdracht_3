402a7d12179a79786ab9cab72b85a054d9c81116
AmaryllisLee <ama.lee@student.hu.nl>
2020-04-18 23:42:01 +0200

    aanpassing collaborative filtering

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,39 +1,65 @@
 import psycopg2
-
+#from content_filtering import fetch_query
 
 c = psycopg2.connect("dbname=huwbbackupdb user=postgres password=amaryllis")
 cur = c.cursor()
 
-#def collaborative_filtering(profilid):
-query = 'select segment from profiles where id = \'5a3a3d91ed295900010450d2\';'
-cur.execute(query)
-records = cur.fetchall()
-for el in records:
-    for seg in el:
-        print(seg)
+def fetch_query(conn, query):
+    'Execute query and return result'
+    cur = conn.cursor()
+    cur.execute(query)
+    fetched = cur.fetchall()
+    cur.close()
+    try:
+        return fetched
+    except:
+        print('Query does not exist')
 
-#Select ids die dezelfde segment heeft als profilid
-query2 = "select id from profiles where segment = '{}';".format(seg)
-cur.execute(query2)
-res = cur.fetchmany(size=4)
-print(res)
+def collaborative(profilid):
+    'Recommend products based on similar profilid'
+
+    #Query that needs to
+
+    segment_prodid = ''' 
+                      --Get the segment of user and the product ids where the user has previously viewed.  
+                     select segment, prodid 
+                     from profiles , profiles_previously_viewed 
+                     where profid = profiles.id And profid ='{}'
+                     '''
 
 
-profile_list = []
-for i in res:
-    for j in i:
-        profile_list.append(j)
-        continue
-print(profile_list)
+    get_profid = '''
+            -- Get the profile_ids which has the same segment and viewed the same product as the 'profilid'
+            select profid from profiles_previously_viewed, profiles where segment = '{}' AND prodid = '{}';
+    '''
 
-rec_prod_id = []
-for prof_id in profile_list:
-    query3 = 'select prodid from profiles_previously_viewed where profid =\'{}\''.format(prof_id)
-    cur.execute(query3)
-    res2 = cur.fetchall()
-    for result in res2:
-        for prod in result:
-            rec_prod_id.append(prod)
-print(rec_prod_id)
+    res_seg_prof = fetch_query(c,segment_prodid.format(profilid))
+    print(res_seg_prof)
+    r = res_seg_prof[0]
+
+    userSegment = r[0]
+    userProdid = r[1]
+
+
+    profids_ids = []
+    result_profids = fetch_query(c, get_profid.format(userSegment, userProdid))
+    for id in result_profids:
+        profids_ids.append(id[0])
+    print(len(profids_ids))
+
+    recommended_prodids = []
+    for profid in profids_ids:
+        getProd = 'select prodid from profiles_previously_viewed where profid = {}; '.format(profid)
+        recom_prod = fetch_query(c, getProd)
+        print(recom_prod)
+        '''
+        if recom_prod == userProdid:
+            continue
+        else:
+        '''
+
+collaborative('5a394475ed29590001038e43')
+
+
 
 
